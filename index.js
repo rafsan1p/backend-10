5487c197d2405ffa2d122f09c94d5be49ff1b417
rafsan1p <rubayetrafsan605@gmail.com>
2025-12-07 03:24:50 +0600

    my order part handle currectly

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        //await client.connect();
+        await client.connect();
 
         const database = client.db('petService');
         const petServices = database.collection('services');
@@ -108,7 +108,7 @@ async function run() {
 
         //npm i mongodb express cors dotenv
 
-        //await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         //await client.close();
